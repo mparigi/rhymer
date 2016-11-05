647a7ec01445861352e3eaafba04e054f1f4a3be
Makarand <mparigi99@gmail.com>
2016-11-04 21:09:01 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("seize the means of production");
+$("#subForm").submit(function (ev) {
+  ev.preventDefault();
+  var w = ev.target.elements.selWord.value;
+  var s = (-4700/9)*(ev.target.elements.speed.value) + (5522);
+  $.getJSON("http://rhymebrain.com/talk?function=getRhymes&word=" + w, function(data) {
+    for (var i = 0; i < 100; i++) window.clearInterval(i); //TERRIBLE
+    var words = data.filter(function(l){
+      return l.word.length > 2 && l.flags === "bc" && l.freq > 18;
+    }).map(function(d){
+      return d.word;
+    });
+    console.log(words);
+    var cnt = 0;
+    $(".toast").remove();
+    Materialize.toast(words[cnt], 7*s);
+    cnt++;
+    window.setInterval(function() {
+      if (!words[cnt]) {
+        for (var i = 0; i < 100; i++) window.clearInterval(i); //TERRIBLE
+        $(".toast").remove();
+        Materialize.toast("<h6>No more rhymes, try another word!</h6>", 10*s);
+        return;
+      }
+      Materialize.toast(words[cnt], 7*s);
+      cnt++;
+    }, s);
+  });
+});
