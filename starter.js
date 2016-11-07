1e061e1732c31cc27a5189455d8e3dc8d2860dd3
Makarand <mparigi99@gmail.com>
2016-11-07 12:57:57 -0500

    file load fix try

--- starter.js
+++ starter.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use("/public", express.static(__dirname + '/'));
 
 app.get("/", function(req, res) {
 	res.sendFile('index.html', { root: __dirname });
